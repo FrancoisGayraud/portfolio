edb217a5bb7a455f3da553dc898e2ab57b4a36bc
François Gayraud <redleader@pc-74.home>
2017-07-08 17:44:17 +0200

    first

---
+++ client/header/header.js
@@ -0,0 +1,25 @@
+import { Template } from 'meteor/templating';
+
+import './header.html';
+import './header.css';
+
+Template.header.helpers({
+    //add you helpers here
+});
+
+Template.header.events({
+    //add your events here
+});
+
+Template.header.onCreated(function () {
+    //add your statement here
+});
+
+Template.header.onRendered(function () {
+    //add your statement here
+});
+
+Template.header.onDestroyed(function () {
+    //add your statement here
+});
+
---
+++ client/home_body/home_body.js
@@ -0,0 +1,25 @@
+import { Template } from 'meteor/templating';
+
+import './home_body.html';
+import './home_body.css';
+
+Template.homeBody.helpers({
+    //add you helpers here
+});
+
+Template.homeBody.events({
+    //add your events here
+});
+
+Template.homeBody.onCreated(function () {
+    //add your statement here
+});
+
+Template.homeBody.onRendered(function () {
+    //add your statement here
+});
+
+Template.homeBody.onDestroyed(function () {
+    //add your statement here
+});
+
